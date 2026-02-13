ff6ee0fcf8a5905a9eb2a228cc0413b4762ebea1
Louis Chouane <chouane.louis@gmail.com>
2026-02-13 14:14:28 +0100

    config lsp java

--- lsp.lua
+++ lsp.lua
@@ -1,7 +1,9 @@
+vim.lsp.set_log_level(vim.log.levels.ERROR)
 vim.diagnostic.config({
     virtual_lines = { current_line = true }
 })
 
+-- RUST LSP --
 vim.lsp.config('rust_ls', {
     cmd = { 'rust-analyzer' },
 
@@ -17,4 +19,28 @@ vim.lsp.config('rust_ls', {
 
 })
 
+-- JAVA LSP --
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = vim.fn.stdpath('cache') .. '/jdtls/' .. project_name
+vim.lsp.config('java_ls', {
+    cmd = {
+        'jdtls',
+        -- workspace directory (per-project is important)
+        '-data', workspace_dir,
+    },
+
+    filetypes = { 'java' },
+
+    root_markers = {
+        'gradlew',
+        'mvnw',
+        'pom.xml',
+        'build.gradle',
+        'build.gradle.kts',
+        '.git',
+    },
+})
+
+-- ACTIVATION --
 vim.lsp.enable('rust_ls')
+vim.lsp.enable('java_ls')
